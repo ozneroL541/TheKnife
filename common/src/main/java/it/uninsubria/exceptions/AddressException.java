4e53fc96c1c1b0624ce8fc0a887df825918196c8
Lorenzo Radice <l.21radice@gmail.com>
2025-06-29 23:54:21 +0200

    Refactor exception classes: remove unnecessary Javadoc inheritance notation from AddressException and UserException

--- common/src/main/java/it/uninsubria/exceptions/AddressException.java
+++ common/src/main/java/it/uninsubria/exceptions/AddressException.java
@@ -4,7 +4,6 @@ package it.uninsubria.exceptions;
  * Custom exception class for handling address-related errors.
  * This exception is thrown when there are issues related to address operations,
  * such as retrieving or inserting address data.
- * @extends RuntimeException
  */
 public class AddressException extends RuntimeException {
     public AddressException(String message) {
--- common/src/main/java/it/uninsubria/exceptions/UserException.java
+++ common/src/main/java/it/uninsubria/exceptions/UserException.java
@@ -4,7 +4,6 @@ package it.uninsubria.exceptions;
  * Custom exception class for handling user-related errors.
  * This exception is thrown when there are issues related to user operations,
  * such as retrieving or inserting user data.
- * @extends RuntimeException
  */
 public class UserException extends RuntimeException {
     public UserException(String message) {
